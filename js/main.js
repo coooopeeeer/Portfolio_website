32266063cf05e571a634688fa91379cab04ecd8b
coooopeeeer <miminashicooper@gmail.com>
2018-12-21 18:03:49 -0800

    Fix typo

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // smooth scroll
 $(function () {
     $('a[href^="#"]').click(function () {
-        var speed = 400;
+        var speed = 1000;
         var href = $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
         var position = target.offset().top;
